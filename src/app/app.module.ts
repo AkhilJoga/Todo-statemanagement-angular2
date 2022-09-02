4525e1d0c38d68c4f47a62dd0850406412e4f846
AkhilJoga <111341918+AkhilJoga@users.noreply.github.com>
2022-09-02 21:38:30 +0530

    Hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AppRoutingModule} from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './Components/todo/todo.component';
+import { AddTodoComponent } from './Components/add-todo/add-todo.component';
+import { TodoReducer } from './reducers/todo.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    AddTodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({todos:TodoReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
